4be5940ac024084fd002f6a27aaa3da2a2164c09
Léncy <87382565+lencydev@users.noreply.github.com>
2022-07-22 11:44:43 +0400

    Update handler.ts

--- src/base/structures/handler.ts
+++ src/base/structures/handler.ts
@@ -1,15 +1,17 @@
 import { Context } from '@context';
 
+import { ClientEvents } from '@discord';
+
 export abstract class Handler {
 
   readonly type;
   readonly enabled;
 
-  constructor(handler: HandlerOptions) {
+  constructor (handler: HandlerOptions) {
 
     this.type = handler.type;
     this.enabled = handler.enabled ?? false;
   };
 
-  abstract execute (ctx: Context, ...items: any[]): Promise<void>;
+  abstract execute (ctx: Context, ...items: ClientEvents[keyof ClientEvents]): Promise<void>;
 };
